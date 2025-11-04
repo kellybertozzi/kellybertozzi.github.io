046c6d453a199e270fd239c9a6bb0a41a45f0ceb
kellybertozzi <152836046+kellybertozzi@users.noreply.github.com>
2025-11-04 17:24:17 -0500

    Updates

--- src/reportWebVitals.js
+++
@@ -1,8 +0,0 @@
-// Minimal reportWebVitals implementation to avoid undefined call
-function reportWebVitals(metric) {
-  if (metric && typeof console !== 'undefined') {
-    console.log(metric);
-  }
-}
-
-export default reportWebVitals;
